3942a17e70430a3c531d7f0d6f3fbcb941fd6815
RenYan Wei <weirenyan@hotmail.com>
2021-08-22 23:10:04 +0800

    init

---
+++ lib/zen.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "zen/version"
+
+module Zen
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/zen/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Zen
+  VERSION = "0.1.0"
+end
