33d34359db95de1c16d4c72ae38384ee0ddc7a5a
RenYan Wei <weirenyan@hotmail.com>
2024-06-04 23:55:24 +0800

    feat: add mvc arch spec feature

--- lib/zen/components/starter_kit.rb
+++ lib/zen/components/starter_kit.rb
@@ -112,13 +112,33 @@ module Zen
         clazz_model.package_name = "#{feature_name}"
         clazz_model.feature_name = feature_name
 
+        # 生成新的文件
+
+        if project.arch_type.downcase == "ddd"
+          puts "use arch type. #{project.arch_type}"
+
+          add_ddd(project, feature_name, table_name, clazz_model, output_root)
+        elsif project.arch_type.downcase == "mvc"
+          puts "use arch type. #{project.arch_type}"
+
+          add_mvc(project, feature_name, table_name, clazz_model, output_root)
+        else
+          puts "not support arch type.#{project.arch_type}"
+        end
+
+        puts "add: #{table_name} done"
+      end
+
+      def add_ddd(project, feature_name, table_name, clazz_model, output_root)
+        # puts "JavaTypes:#{StarterKit::Model::JavaTypes.value("varchar".upcase.to_sym)}"
+        # puts "settings  #{settings.options.verbose}"
+
+        clazz_model.package_name = "#{feature_name}"
+        clazz_model.feature_name = feature_name
+
         # puts "#{table_name} create class : #{pp clazz_model}"
 
-        @user_template = File.join(USER_TEMPLATE_ROOT, "starter/ddd_init")
-
-        @template = File.join(TEMPLATE_ROOT, "starter/ddd_init")
-
-        File.exist?(@template)
+        template_base = load_template("starter/ddd_spec")
 
         class_template_name = "model.erb"
         class_template_name = "entity.java.erb"
@@ -297,7 +317,7 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Mapper.xml")
 
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
 
         # Entity Module Process template
         template_name = entity_module.module_template
@@ -307,7 +327,7 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Entity.java")
 
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
 
         # Mapper Module process template
         template_name = mapper_module.module_template
@@ -317,7 +337,7 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Mapper.java")
 
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
 
         # Entity Convert Module process template
         template_name = entity_convert_module.module_template
@@ -327,7 +347,7 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}EntityConvert.java")
 
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
 
         # Model Module process template
         template_name = model_module.module_template
@@ -337,7 +357,7 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}DO.java")
 
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
 
         # Repository Module process template
         template_name = repository_module.module_template
@@ -347,7 +367,7 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Repository.java")
 
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
 
         # Repository Impl Module process template
         template_name = repository_impl_module.module_template
@@ -357,7 +377,267 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}RepositoryImpl.java")
 
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
+
+        # Repository Impl Module process template
+        template_name = view_module.module_template
+
+        output_path = File.join(output_root,
+                                view_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Vo.java")
+        process_template(template_base, template_name, output_path, context)
+
+        # 生成新的文件
+
+        puts "add: #{table_name} done"
+      end
+
+      def add_mvc(project, feature_name, table_name, clazz_model, output_root)
+        # puts "JavaTypes:#{StarterKit::Model::JavaTypes.value("varchar".upcase.to_sym)}"
+        # puts "settings  #{settings.options.verbose}"
+
+        clazz_model.package_name = "#{feature_name}"
+        clazz_model.feature_name = feature_name
+
+        # puts "#{table_name} create class : #{pp clazz_model}"
+
+        template_base = load_template("starter/mvc_spec")
+
+        # Entity class
+        # @example
+        #   __app__/__app__-domain/src/main/java/__package__/domain/entity
+        entity_module = Zen::Components::StarterKit::Model::JavaModule.new
+
+        entity_module.module_name = "ENTITY"
+        entity_module.module_template = "entity.java.erb"
+        entity_module.project = project
+        entity_module.module_package = "domain.entity"
+        entity_module.module_path = "#{project.project_name}-domain"
+        entity_module.module_type = "java"
+
+        # Mapper Interface Module
+        # @example
+        #   __app__/__app__-domain/src/main/java/__package__/domain/mapper
+        #   __app__/__app__-domain/src/main/java/__package__/domain/mapper/user/xxxMapper.java
+        # @type [Model::JavaModule]
+        mapper_module = Zen::Components::StarterKit::Model::JavaModule.new
+
+        mapper_module.module_name = "MAPPER"
+        mapper_module.module_template = "dao.java.erb"
+        mapper_module.project = project
+        mapper_module.module_package = "domain.mapper"
+        mapper_module.module_path = "#{project.project_name}-domain"
+        mapper_module.module_type = "java"
+        mapper_module.module_suffix = "Mapper"
+        mapper_module.module_output = "Mapper.java"
+
+        # Mapper Resource Module
+        # __app__/__app__-domain/src/main/resources/mapper/xxxMapper.xml
+        mapper_res_module = Zen::Components::StarterKit::Model::JavaModule.new
+
+        mapper_res_module.module_name = "MAPPER_RESOURCE"
+        mapper_res_module.module_template = "mapper.xml.erb"
+        mapper_res_module.project = project
+        mapper_res_module.module_package = "mapper"
+        mapper_res_module.module_path = "#{project.project_name}-domain"
+        mapper_res_module.module_type = "resource"
+        mapper_res_module.module_suffix = "Mapper"
+        mapper_res_module.module_output = "Mapper.xml"
+
+        # Entity Convert Module
+        #
+        # @example
+        #      __app__/__app__-domain/src/main/java/__package__/domain/convert
+        entity_convert_module = Zen::Components::StarterKit::Model::JavaModule.new
+
+        entity_convert_module.module_name = "ENTITY_CONVERT"
+        entity_convert_module.module_template = "entity_convert.java.erb"
+        entity_convert_module.project = project
+        entity_convert_module.module_package = "domain.convert"
+        entity_convert_module.module_path = "#{project.project_name}-domain"
+        entity_convert_module.module_type = Model::JavaModule::SOURCE_TYPE
+        entity_convert_module.module_suffix = "EntityConvert"
+        entity_convert_module.module_output = "EntityConvert.java"
+
+        # Domain Model Module
+        #
+        # @example
+        #      __app__/__app__-domain/src/main/java/__package__/domain/model
+        #      __app__/__app__-domain/src/main/java/__package__/domain/model/sku/SkuDO.java
+        model_module = Zen::Components::StarterKit::Model::JavaModule.new
+
+        model_module.module_name = "MODEL"
+        model_module.module_template = "model.java.erb"
+        model_module.project = project
+        model_module.module_package = "domain.model"
+        model_module.module_path = "#{project.project_name}-domain"
+        model_module.module_type = Model::JavaModule::SOURCE_TYPE
+        model_module.module_suffix = "DO"
+        model_module.module_output = "DO.java"
+
+        # Repository Module
+        #
+        # @example
+        # __app__/__app__-domain/src/main/java/__package__/domain/repository/sku
+        # __app__/__app__-domain/src/main/java/__package__/domain/repository/sku/SkuExampleRepository.java
+        #
+        repository_module = Zen::Components::StarterKit::Model::JavaModule.new
+
+        repository_module.module_name = "REPOSITORY"
+        repository_module.module_template = "repository.java.erb"
+        repository_module.project = project
+        repository_module.module_package = "domain.repository"
+        repository_module.module_path = "#{project.project_name}-domain"
+        repository_module.module_type = Model::JavaModule::SOURCE_TYPE
+        repository_module.module_suffix = "Repository"
+        repository_module.module_output = "Repository.java"
+        # Repository Impl Module
+        #
+        # @example
+        #   __app__/__app__-infrastructure/src/main/java/__package__/infrastructure/repository
+        #   __app__/__app__-infrastructure/src/main/java/__package__/infrastructure/repository/sku/xxxRepositoryImpl.java
+        repository_impl_module = Zen::Components::StarterKit::Model::JavaModule.new
+
+        repository_impl_module.module_name = "REPOSITORY_IMPL"
+        repository_impl_module.module_template = "repository_impl.java.erb"
+        repository_impl_module.project = project
+        repository_impl_module.module_package = "domain.repository"
+        repository_impl_module.module_path = "#{project.project_name}-domain"
+        repository_impl_module.module_type = Model::JavaModule::SOURCE_TYPE
+        repository_impl_module.module_suffix = "RepositoryImpl"
+        repository_impl_module.module_output = "RepositoryImpl.java"
+
+        pp repository_impl_module.model(clazz_model)
+
+        #
+        # __app__/__app__-domain/src/main/java/__package__/domain/service
+        # __app__/__app__-domain/src/main/java/__package__/domain/service/sku/SkuService.java
+        service_module = Zen::Components::StarterKit::Model::JavaModule.new
+        service_module.module_name = "SERVICE"
+        service_module.module_template = "service.java.erb"
+        service_module.project = project
+        service_module.module_package = "domain.service"
+        service_module.module_path = "#{project.project_name}-domain"
+        service_module.module_type = Model::JavaModule::SOURCE_TYPE
+        service_module.module_suffix = "Service"
+        service_module.module_output = "Service.java"
+
+        # __app__/__app__-domain/src/main/java/__package__/domain/service/sku/impl/SkuServiceImpl.java
+        #
+        service_impl_module = Zen::Components::StarterKit::Model::JavaModule.new
+        service_impl_module.module_name = "SERVICE_IMPL"
+        service_impl_module.module_template = "service_impl.java.erb"
+        service_impl_module.project = project
+        service_impl_module.module_package = "impl"
+        service_impl_module.module_path = "#{project.project_name}-domain"
+        service_impl_module.module_type = Model::JavaModule::SOURCE_TYPE
+        service_impl_module.module_suffix = "ServiceImpl"
+        service_impl_module.module_output = "ServiceImpl.java"
+
+        # __app__/__app__-domain/src/main/java/__package__/domain/vo
+        # __app__/__app__-domain/src/main/java/__package__/domain/vo/sku/SkuDetailVo.java
+        #
+
+        view_module = Zen::Components::StarterKit::Model::JavaModule.new
+        view_module.module_name = "VIEW"
+        view_module.module_template = "vo.java.erb"
+        view_module.project = project
+        view_module.module_package = "domain.vo"
+        view_module.module_path = "#{project.project_name}-domain"
+        view_module.module_type = Model::JavaModule::SOURCE_TYPE
+        view_module.module_suffix = "Vo"
+        view_module.module_output = "Vo.java"
+
+        ##
+        # @type [Hash(String,JavaModule)]
+        modules = { entity: entity_module,
+                    mapper: mapper_module,
+                    mapper_resource: mapper_res_module,
+                    entity_convert: entity_convert_module,
+                    model: model_module,
+                    repository: repository_module,
+                    repository_impl: repository_impl_module,
+                    service: service_module,
+                    service_impl: service_impl_module,
+                    vo: view_module }
+
+        # Prepare template data
+        context = Model::JavaContext.new
+        context.project = project
+        context.model = clazz_model
+        context.modules = modules
+        # pp context
+
+        # Mapper MyBatis Module Process template
+        template_name = mapper_res_module.module_template
+
+        output_path = File.join(output_root,
+                                mapper_res_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Mapper.xml")
+
+        process_template(template_base, template_name, output_path, context)
+
+        # Entity Module Process template
+        template_name = entity_module.module_template
+
+        output_path = File.join(output_root,
+                                entity_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Entity.java")
+
+        process_template(template_base, template_name, output_path, context)
+
+        # Mapper Module process template
+        template_name = mapper_module.module_template
+
+        output_path = File.join(output_root,
+                                mapper_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Mapper.java")
+
+        process_template(template_base, template_name, output_path, context)
+
+        # Entity Convert Module process template
+        template_name = entity_convert_module.module_template
+
+        output_path = File.join(output_root,
+                                entity_convert_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}EntityConvert.java")
+
+        process_template(template_base, template_name, output_path, context)
+
+        # Model Module process template
+        template_name = model_module.module_template
+
+        output_path = File.join(output_root,
+                                model_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}DO.java")
+
+        process_template(template_base, template_name, output_path, context)
+
+        # Repository Module process template
+        template_name = repository_module.module_template
+
+        output_path = File.join(output_root,
+                                repository_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Repository.java")
+
+        process_template(template_base, template_name, output_path, context)
+
+        # Repository Impl Module process template
+        template_name = repository_impl_module.module_template
+
+        output_path = File.join(output_root,
+                                repository_impl_module.full_path,
+                                clazz_model.package_name,
+                                "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}RepositoryImpl.java")
+
+        process_template(template_base, template_name, output_path, context)
 
         # Repository Impl Module process template
         template_name = view_module.module_template
@@ -367,13 +647,32 @@ module Zen
                                 clazz_model.package_name,
                                 "#{clazz_model.feature_name.camelcase}#{clazz_model.class_name}Vo.java")
 
-        template_base = "starter/ddd_spec"
-
-        process_template(template_name, output_path, context)
+        process_template(template_base, template_name, output_path, context)
 
         # 生成新的文件
 
         puts "add: #{table_name} done"
+      end
+
+      ##
+      # load template base from template root
+      def load_template(template_base)
+        # template root directory
+        templates_root = File.join(Zen::TEMPLATE_ROOT, template_base)
+
+        if !Dir.exist?(templates_root) && Application.logger.debug?
+          Application.logger.debug "JavaStarter: template not exists ."
+        end
+
+        # use template root directory
+        user_template = File.join(Zen::USER_TEMPLATE_ROOT, template_base)
+
+        if Dir.exist?(user_template)
+          templates_root = user_template
+          Application.logger.debug "JavaScaffold: user template exists, use this template." if Application.logger.debug?
+        end
+
+        templates_root
       end
 
       require "erb"
@@ -381,8 +680,8 @@ module Zen
 
       # 定义一个方法来处理模板替换
       # @api private
-      def process_template(template_name, output_path, context)
-        template_path = File.join(Zen::TEMPLATE_ROOT, "starter/ddd_spec", template_name)
+      def process_template(template_base, template_name, output_path, context)
+        template_path = File.join(template_base, template_name)
         puts "template name :#{template_path}" if settings.options.verbose
 
         # 读取模板文件内容
@@ -492,7 +791,7 @@ module Zen
           end
 
           # use template root directory
-          user_template = File.join(USER_TEMPLATE_ROOT, TEMPLATE_NAME)
+          user_template = File.join(Zen::USER_TEMPLATE_ROOT, TEMPLATE_NAME)
 
           if Dir.exist?(user_template)
             templates_root = user_template
