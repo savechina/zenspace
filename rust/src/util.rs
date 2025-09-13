740763fffc957e83a4b4e955004b430ab8e222d5
RenYan Wei <weirenyan@hotmail.com>
2025-09-14 00:16:43 +0800

    feat: update include dir

--- rust/src/util.rs
+++ rust/src/util.rs
@@ -2,6 +2,14 @@ use std::path::PathBuf;
 
 use which;
 
+use include_dir::{Dir, include_dir};
+
+/// Assets
+static ASSETS: Dir = include_dir!("assets");
+
+/// Template
+static TEMPLATES: Dir = include_dir!("templates");
+
 /// which `command` exists
 pub(crate) fn command_exists(command: &str) -> bool {
     let result = which::which(command);
